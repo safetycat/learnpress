3212ac46c7d621f87a6541a0df99a4d22805fec0
James Cat <zz.james@gmail.com>
2014-06-20 19:41:36 +0100

    actual plug in file

---
+++ learnpress.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: Hello Reader
+Plugin URI: http://github.com/tommcfarlin/Hello-Reader
+Description: A simple plugin used to help demonstrate unit testing in the context of WordPress.
+Version: 1.0
+Author: Tom McFarlin
+Author URI: http://tom.mcfarl.in
+Author Email: tom@tommcfarlin.com
+License:
+ 
+  Copyright 2012 Tom McFarlin (tom@tommcfarlin.com)
+ 
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+   
+*/
+// Only create an instance of the plugin if it doesn't already exists in GLOBALS
+if( ! array_key_exists( 'hello-reader', $GLOBALS ) ) {
+ 
+    class Hello_Reader {
+          
+        function __construct() {
+             
+        } // end constructor
+       
+    } // end class
+     
+    // Store a reference to the plugin in GLOBALS so that our unit tests can access it
+    $GLOBALS['hello-reader'] = new Hello_Reader();
+     
+} // end if
